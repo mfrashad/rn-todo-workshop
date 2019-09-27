69da69e82c800afb2ef18b562b83c7812bf5eda2
Rashad <m.fathyrashad@gmail.com>
2019-09-28 07:17:43 +0800

    Card view

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 import { LinearGradient }  from 'expo-linear-gradient';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
   return (
     <LinearGradient style={styles.container} colors={['#DA4453', '#89216B']}>
       <Text style={styles.appTitle}>Minimalist Todo App</Text>
+      <View style={styles.card}>
+        <TextInput placeholder="Add an Item"></TextInput>
+      </View>
     </LinearGradient>
   );
 }
@@ -23,5 +28,18 @@ const styles = StyleSheet.create({
     marginTop: 60,
     marginBottom: 30,
     fontWeight: '300'
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24
   }
 });
