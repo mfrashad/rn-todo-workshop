cddfc3ad8cc5eba897497ac66faacf30a4960262
Rashad <m.fathyrashad@gmail.com>
2019-09-28 08:00:02 +0800

    Adding local state for new todo item

--- App.js
+++ App.js
@@ -4,15 +4,34 @@ import { LinearGradient }  from 'expo-linear-gradient';
 
 const { height, width } = Dimensions.get('window');
 
-export default function App() {
-  return (
-    <LinearGradient style={styles.container} colors={['#DA4453', '#89216B']}>
-      <Text style={styles.appTitle}>Minimalist Todo App</Text>
-      <View style={styles.card}>
-        <TextInput placeholder="Add an Item"></TextInput>
-      </View>
-    </LinearGradient>
-  );
+export default class App extends React.Component {
+  state = {
+    newTodoItem: ''
+  }
+
+  newTodoItemController = textValue => {
+    this.setState({newTodoItem: textValue})
+  }
+
+  render() {
+    const { newTodoItem } = this.state
+    return (
+      <LinearGradient style={styles.container} colors={['#DA4453', '#89216B']}>
+        <Text style={styles.appTitle}>Minimalist Todo App</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input}
+            placeholder="Add an Item"
+            value={newTodoItem}
+            onChangeText={this.newTodoItemController}
+            placeholderTextColor="#999"
+            returnKeyType="done"
+            autoCorrect="false"
+          ></TextInput>
+        </View>
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
